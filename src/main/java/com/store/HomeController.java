9553b27fc500db6fefe1cdab332b3940d3feb3f8
Chaz Selph <csselph@gmail.com>
2019-07-16 21:16:36 -0600

    ip

---
+++ src/main/java/com/store/HomeController.java
@@ -0,0 +1,31 @@
+package com.store;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+class HomeController{
+
+	 @GetMapping("/")
+	 @ResponseBody
+	 public HttpEntity<MessageService> message(@RequestParam(value = "name",
+	 	required = false, defaultValue = "user") String name) {
+
+		 MessageService message = new MessageService();
+		 message.setMessage("Hello " + name);
+
+	 	 message.add(linkTo(methodOn(HomeController.class)
+	 			.message(name))
+	 			.withSelfRel());
+
+	 	 return new ResponseEntity<>(message, HttpStatus.OK);
+	 }
+
+}
